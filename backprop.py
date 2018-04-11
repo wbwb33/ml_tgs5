4127ccaacf4f84091a715bf52b437e5157a2db8e
wibisw <wibi.s.wi@gmail.com>
2018-04-12 05:39:20 +0700

    Update backprop.py

--- backprop.py
+++ backprop.py
@@ -5,358 +5,242 @@ Spyder Editor
 This is a temporary script file.
 """
 import numpy as np
-import re
 import pandas as pd
-from math import exp
 import matplotlib.pyplot as plt
 
-data = 150
-a = 0.01
-epoch = 100
+iris = pd.read_csv("iris.csv", header=None)
 
-x1 = np.empty(data)
-x2 = np.empty(data)
-x3 = np.empty(data)
-x4 = np.empty(data)
-kelas = np.empty(data, dtype='S30')
+w15=pd.Series([])
+w25=pd.Series([])
+w35=pd.Series([])
+w45=pd.Series([])
+w16=pd.Series([])
+w26=pd.Series([])
+w36=pd.Series([])
+w46=pd.Series([])
+b5=pd.Series([])
+b6=pd.Series([])
+w57=pd.Series([])
+w67=pd.Series([])
+w58=pd.Series([])
+w68=pd.Series([])
+w59=pd.Series([])
+w69=pd.Series([])
+b7=pd.Series([])
+b8=pd.Series([])
+b9=pd.Series([])
+#inisialisasi
+w15[0]=0.14
+w25[0]=0.39
+w35[0]=0.55
+w45[0]=0.72
+w16[0]=0.11
+w26[0]=0.34
+w36[0]=0.31
+w46[0]=0.65
+b5[0]=0.70
+b6[0]=0.20
+w57[0]=0.51
+w67[0]=0.44
+w58[0]=0.29
+w68[0]=0.09
+w59[0]=0.87
+w69[0]=0.79
+b7[0]=0.05
+b8[0]=0.91
+b9[0]=0.54
+a=0.1 #learningrate
+epoch=100 #epoch
 
-fakta1 = np.empty(data, dtype='int32')
-fakta2 = np.empty(data, dtype='int32')
-fakta3 = np.empty(data, dtype='int32')
+#fungsi-fungsi
+def y1(x1,x2,x3,x4,w1,w2,w3,w4,b):
+    return (w1*x1)+(w2*x2)+(w3*x3)+(w4*x4)+b
 
-fakta1[0:50] = 1
-fakta1[50:150] = 0
-fakta2[0:50] = 0
-fakta2[50:100] = 1
-fakta2[100:150] = 0
-fakta3[0:100] = 0
-fakta3[100:150] = 1
-
-def get_data(dt,grp):
-	f = open('iris.txt','r')
-	f1 = f.readlines()
-	p = re.compile(r'([\d\.]+),([\d\.]+),([\d\.]+),([\d\.]+),([\w\-]+)')
-	for i in range(data):
-		dt[i] = p.match(f1[i]).group(grp)
-	f.close()
-	return dt
-
-def signoid(x):
-    return 1 / (1 + exp(-x))
-
-def table_count(data):
-    return data.iloc[:,0].size
-
-def theta_bias_initiation():
-    theta1 = np.array([[0.88, 0.36, 0.03, 0.57], 
-                       [0.02, 0.93, 0.54, 0.32],
-                       [0.57, 0.03, 0.99, 0.88]])
-    bias1 = np.array([0.39,0.93,0.27])
-
-    theta2 = np.array([[0.82, 0.3, 0.64], 
-                       [0.65, 0.2, 0.52],
-                       [0.7, 0.55, 0.18]])
-    bias2 = np.array([0.94,0.56,0.55])
-
-    return [theta1, bias1, theta2, bias2]
-
-def back_prop(training,params):
-    theta1 = np.array(params[0])
-    bias1 = np.array(params[1])
-    theta2 = np.array(params[2])
-    bias2 = np.array(params[3])
+def y2(x1,x2,w1,w2,b):
+    return (w1*x1)+(w2*x2)+b
     
-    error1 = 0
-    error2 = 0
-    error3 = 0
+def sigmoid_activation(x):
+	return 1.0 / (1 + np.exp(-x))    
 
-    for i in range(table_count(training)):
-      
-        h11 = sum(training.iloc[i,4:8]*theta1[0,0:4]) + bias1[0]
-        h12 = sum(training.iloc[i,4:8]*theta1[1,0:4]) + bias1[1]
-        h13 = sum(training.iloc[i,4:8]*theta1[2,0:4]) + bias1[2]
-       
-        s11 = signoid(h11)
-        s12 = signoid(h12)
-        s13 = signoid(h13)
-    
-        h21 = s11 * theta2[0][0] + s12 * theta2[0][1] + s13 * theta2[0][2] + bias2[0]
-        h22 = s11 * theta2[1][0] + s12 * theta2[1][1] + s13 * theta2[1][2] + bias2[1]
-        h23 = s11 * theta2[2][0] + s12 * theta2[2][1] + s13 * theta2[2][2] + bias2[2]
+def tetabaru(t,a,taw,pred):
+    return t-(a*taw*pred)
 
-        s21 = signoid(h21)
-        s22 = signoid(h22)
-        s23 = signoid(h23)
+def taw_out(pred,fact):
+    return (pred-fact)*(1-pred)*pred
 
-        error1 = error1 + (s21 - training.iloc[i,0])**2
-        error2 = error2 + (s22 - training.iloc[i,1])**2
-        error3 = error3 + (s23 - training.iloc[i,2])**2
+def taw_hidd(w1,w2,w3,taw1,taw2,taw3,pred):
+    return ((taw1*w1)+(taw2*w2)+(taw3*w3))*(1-pred)*pred
 
-        tau21 = 2*(s21 - training.iloc[i,0]) * (1-s21)*s21
-        dw21 = tau21 * s11
-        dw24 = tau21 * s12
-        dw27 = tau21 * s13
-
-    
-        tau22 = 2*(s22 - training.iloc[i,1]) * (1-s22)*s22
-        dw22 = tau22 * s11
-        dw25 = tau22 * s12
-        dw28 = tau22 * s13
-        
-        tau23 = 2*(s23 - training.iloc[i,2]) * (1-s23) * s23
-        dw23 = tau23 * s11
-        dw26 = tau23 * s12
-        dw29 = tau23 * s13
-        
-        tau11 = (tau21 * theta2[0,0] + tau22 * theta2[1,0] + tau23 * theta2[2,0]) * (1-s11) * s11
-        dw11 = tau11 * training.iloc[i,4]
-        dw14 = tau11 * training.iloc[i,5]
-        dw17 = tau11 * training.iloc[i,6]
-        dw110 = tau11 * training.iloc[i,7]
-
-        tau12 = (tau21 * theta2[0,1] + tau22 * theta2[1,1] + tau23 * theta2[2,1]) *(1-s12)*s12
-        dw12 = tau12 * training.iloc[i,4]
-        dw15 = tau12 * training.iloc[i,5]
-        dw18 = tau12 * training.iloc[i,6]
-        dw111 = tau12 * training.iloc[i,7]
-        
-        tau13 = (tau21 * theta2[0,2] + tau22 * theta2[1,2] + tau23 * theta2[2,2]) * (1-s13) * s13
-        dw13 = tau13 * training.iloc[i,4]
-        dw16 = tau13 * training.iloc[i,5]
-        dw19 = tau13 * training.iloc[i,6]
-        dw112 = tau13 * training.iloc[i,7]
-    
-        dw2 = np.array([[dw21, dw24, dw27],
-                        [dw22, dw25, dw28],
-                        [dw23, dw26, dw29]])
-
-        dbias2 = np.array([tau21, tau22, tau23])
-  
-        dw1 = np.array([[dw11, dw14, dw17, dw110],
-                        [dw12, dw15, dw18, dw111],
-                        [dw13, dw16, dw19, dw112]])
-     
-        dbias1 = np.array([tau11, tau12, tau13])
-      
-        theta1 = theta1 - a * dw1
-        bias1 = bias1 - a * dbias1
-        theta2 = theta2 - a * dw2
-        bias2 = bias2 - a * dbias2
-        
-    error = np.array([error1, error2,error3])
-    error = error/table_count(training)
-    error = np.mean(error)
-
-    
-    return np.array([[error],[theta1, bias1, theta2, bias2]])
-
-def predict(signoid):
-    if(signoid < 0.5):
-        return 0
+#cleaning data
+#setosa = 0
+#versicolor = 1
+# = 2
+for x in range(0,len(iris)):
+    if iris.iloc[x,4] in ['Iris-setosa']:
+        iris.iloc[x,4]=0
+    elif iris.iloc[x,4] in ['Iris-versicolor']:
+        iris.iloc[x,4]=1
     else:
-        return 1
-
-def checking_accuration(data1,data2):
-    checking = np.zeros(table_count(data1), dtype=bool)
-
-    for i in range(table_count(data1)):
+        iris.iloc[x,4]=2
         
-        checking[i] = np.array_equal(data1.iloc[i].values, data2.iloc[i].values)
-    
-    return checking
+#preparation for training dataset
+x1 = iris.iloc[:,0]
+x2 = iris.iloc[:,1]
+x3 = iris.iloc[:,2]
+x4 = iris.iloc[:,3]
+label = iris.iloc[:,4]
+avg_error = pd.Series([])
+in5 = pd.Series([])
+in6 = pd.Series([])
+out5 = pd.Series([])
+out6 = pd.Series([])
+taw5 = pd.Series([])
+taw6 = pd.Series([])
+in7 = pd.Series([])
+in8 = pd.Series([])
+in9 = pd.Series([])
+out7 = pd.Series([])
+out8 = pd.Series([])
+out9 = pd.Series([])
+taw7 = pd.Series([])
+taw8 = pd.Series([])
+taw9 = pd.Series([])
+error = pd.Series([])
 
-def test(testing, params):
-    theta1 = np.array(params[0])
-    bias1 = np.array(params[1])
-    theta2 = np.array(params[2])
-    bias2 = np.array(params[3])
-    
-    prediksi1 = np.zeros(table_count(testing), dtype='int32')
-    prediksi2 = np.zeros(table_count(testing), dtype='int32')
-    prediksi3 = np.zeros(table_count(testing), dtype='int32')
 
-    error1 = 0
-    error2 = 0
-    error3 = 0
-    
-    for i in range(table_count(testing)):
-    
-        h11 = sum(testing.iloc[i,4:8]*theta1[0,0:4]) + bias1[0]
-        h12 = sum(testing.iloc[i,4:8]*theta1[1,0:4]) + bias1[1]
-        h13 = sum(testing.iloc[i,4:8]*theta1[2,0:4]) + bias1[2]
-    
-        s11 = signoid(h11)
-        s12 = signoid(h12)
-        s13 = signoid(h13)
-    
-        h21 = s11 * theta2[0][0] + s12 * theta2[0][1] + s13 * theta2[0][2] + bias2[0]
-        h22 = s11 * theta2[1][0] + s12 * theta2[1][1] + s13 * theta2[1][2] + bias2[1]
-        h23 = s11 * theta2[2][0] + s12 * theta2[2][1] + s13 * theta2[2][2] + bias2[2]
-    
-        s21 = signoid(h21)
-        s22 = signoid(h22)
-        s23 = signoid(h23)
+def feed(data):
+    global w15,w25,w35,w45,w16,w26,w36,w46,b5,b6,w57,w67,w58,w68,w59,w69,b7,b8,b9
+    for z in range(0,len(data)):
+        in5[z] = y1(x1[z],x2[z],x3[z],x4[z],w15[z],w25[z],w35[z],w45[z],b5[z])
+        in6[z] = y1(x1[z],x2[z],x3[z],x4[z],w16[z],w26[z],w36[z],w46[z],b6[z])
+        out5[z] = sigmoid_activation(in5[z])
+        out6[z] = sigmoid_activation(in6[z])
+        in7[z] = y2(out5[z],out6[z],w57[z],w67[z],b7[z])
+        in8[z] = y2(out5[z],out6[z],w58[z],w68[z],b8[z])
+        in9[z] = y2(out5[z],out6[z],w59[z],w69[z],b9[z])
+        out7[z] = sigmoid_activation(in7[z])
+        out8[z] = sigmoid_activation(in8[z])
+        out9[z] = sigmoid_activation(in9[z])
+        if label[z]==0:
+            error[z] = ((1-out7[z])**2 + (0-out8[z])**2 + (0-out9[z])**2)/3
+            taw7[z] = taw_out(out7[z],1)
+            taw8[z] = taw_out(out8[z],0)
+            taw9[z] = taw_out(out9[z],0)
+        elif label[z]==1:
+            error[z] = ((0-out7[z])**2 + (1-out8[z])**2 + (0-out9[z])**2)/3
+            taw7[z] = taw_out(out7[z],0)
+            taw8[z] = taw_out(out8[z],1)
+            taw9[z] = taw_out(out9[z],0)
+        else:
+            error[z] = ((0-out7[z])**2 + (0-out8[z])**2 + (1-out9[z])**2)/3
+            taw7[z] = taw_out(out7[z],0)
+            taw8[z] = taw_out(out8[z],0)
+            taw9[z] = taw_out(out9[z],1)
+        w57[z+1]=tetabaru(w57[z],a,taw7[z],out5[z])
+        w67[z+1]=tetabaru(w67[z],a,taw7[z],out6[z])
+        w58[z+1]=tetabaru(w58[z],a,taw8[z],out5[z])
+        w68[z+1]=tetabaru(w68[z],a,taw8[z],out6[z])
+        w59[z+1]=tetabaru(w59[z],a,taw9[z],out5[z])
+        w69[z+1]=tetabaru(w69[z],a,taw9[z],out6[z])
+        b7[z+1]=tetabaru(b7[z],a,taw7[z],1)
+        b8[z+1]=tetabaru(b8[z],a,taw8[z],1)
+        b9[z+1]=tetabaru(b8[z],a,taw9[z],1)
+        taw5[z]=taw_hidd(w57[z],w58[z],w59[z],taw7[z],taw8[z],taw9[z],out5[z])
+        taw6[z]=taw_hidd(w67[z],w68[z],w69[z],taw7[z],taw8[z],taw9[z],out6[z])
+        w15[z+1]=tetabaru(w15[z],a,taw5[z],x1[z])
+        w25[z+1]=tetabaru(w25[z],a,taw5[z],x2[z])
+        w35[z+1]=tetabaru(w35[z],a,taw5[z],x3[z])
+        w45[z+1]=tetabaru(w45[z],a,taw5[z],x4[z])
+        w16[z+1]=tetabaru(w16[z],a,taw6[z],x1[z])
+        w26[z+1]=tetabaru(w26[z],a,taw6[z],x2[z])
+        w36[z+1]=tetabaru(w36[z],a,taw6[z],x3[z])
+        w46[z+1]=tetabaru(w46[z],a,taw6[z],x4[z])
+        b5[z+1]=tetabaru(b5[z],a,taw5[z],1)
+        b6[z+1]=tetabaru(b6[z],a,taw6[z],1)   
+    w15[0]=w15[len(data)]
+    w25[0]=w25[len(data)]
+    w35[0]=w35[len(data)]
+    w45[0]=w45[len(data)]
+    w16[0]=w16[len(data)]
+    w26[0]=w26[len(data)]
+    w36[0]=w36[len(data)]
+    w46[0]=w46[len(data)]
+    b5[0]=b5[len(data)]
+    b6[0]=b6[len(data)]
+    w57[0]=w57[len(data)]
+    w67[0]=w67[len(data)]
+    w58[0]=w58[len(data)]
+    w68[0]=w68[len(data)]
+    w59[0]=w59[len(data)]
+    w69[0]=w69[len(data)]
+    b7[0]=b7[len(data)]
+    b8[0]=b8[len(data)]
+    b9[0]=b9[len(data)]
+    avg_error = np.mean(error)
+    return avg_error
 
-        prediksi1[i] = predict(s21)
-        prediksi2[i] = predict(s22)
-        prediksi3[i] = predict(s23)
-    
-        error1 = error1 + (s21 - testing.iloc[i,0])**2
-        error2 = error2 + (s22 - testing.iloc[i,1])**2
-        error3 = error3 + (s23 - testing.iloc[i,2])**2
-        
-    error = np.array([error1, error2,error3])
-    error = error/table_count(testing)
-    error = np.mean(error)
-    
-    predict_table = pd.DataFrame({'prediksi 1':prediksi1,
-                                  'prediksi 2':prediksi2,
-                                  'prediksi 3':prediksi3})
-    
-    text.append(str(predict_table)+"\n")
-  
-    conditional = checking_accuration(testing.iloc[:,0:3], predict_table)
-    
-    unique, count = np.unique(conditional, return_counts=True)
-    
-    c = np.where(unique == True)
-    if(c[0].size != 0):   
-        akurasi = (float(count[c[0][0]]) / table_count(testing)) * 100
-        text.append("Akurasi "+str(akurasi)+"\n")
-    else:
-        akurasi = 0
-        text.append("Akurasi "+str(akurasi)+"\n")
-    
-    return np.array([error,akurasi])
+def test(data):
+    for z in range(0,len(data)):
+        in5[z] = y1(x1[z],x2[z],x3[z],x4[z],w15[0],w25[0],w35[0],w45[0],b5[0])
+        in6[z] = y1(x1[z],x2[z],x3[z],x4[z],w16[0],w26[0],w36[0],w46[0],b6[0])
+        out5[z] = sigmoid_activation(in5[z])
+        out6[z] = sigmoid_activation(in6[z])
+        in7[z] = y2(out5[z],out6[z],w57[0],w67[0],b7[0])
+        in8[z] = y2(out5[z],out6[z],w58[0],w68[0],b8[0])
+        in9[z] = y2(out5[z],out6[z],w59[0],w69[0],b9[0])
+        out7[z] = sigmoid_activation(in7[z])
+        out8[z] = sigmoid_activation(in8[z])
+        out9[z] = sigmoid_activation(in9[z])
+        if label[z]==0:
+            error[z] = ((1-out7[z])**2 + (0-out8[z])**2 + (0-out9[z])**2)/3
+        elif label[z]==1:
+            error[z] = ((0-out7[z])**2 + (1-out8[z])**2 + (0-out9[z])**2)/3
+        else:
+            error[z] = ((0-out7[z])**2 + (0-out8[z])**2 + (1-out9[z])**2)/3
+    avg_error = np.mean(error)
+    return avg_error
 
-f = open('output-back.txt','w')
-text = []
+error_train=pd.Series([])
+error_test=pd.Series([])  
 
-x1 = get_data(x1,1)
-x2 = get_data(x2,2)
-x3 = get_data(x3,3)
-x4 = get_data(x4,4)
-kelas = get_data(kelas,5)
+for cur_epoch in range(0,epoch):
+    #1
+    training = iris.iloc[30:150]
+    testing = iris.iloc[0:30]
+    error_train[cur_epoch]=feed(training)
+    error_test[cur_epoch]=test(testing)
+    #2
+    training = iris.iloc[0:30]
+    training = training.append(iris.iloc[60:150])
+    testing = iris.iloc[30:60]
+    error_train[cur_epoch]=error_train[cur_epoch]+feed(training)
+    error_test[cur_epoch]=error_test[cur_epoch]+test(testing)
+    #3
+    training = iris.iloc[0:60]
+    training = training.append(iris.iloc[90:150])
+    testing = iris.iloc[60:90]
+    error_train[cur_epoch]=error_train[cur_epoch]+feed(training)
+    error_test[cur_epoch]=error_test[cur_epoch]+test(testing)
+    #4
+    training = iris.iloc[0:90]
+    training = training.append(iris.iloc[120:150])
+    testing = iris.iloc[90:120]
+    error_train[cur_epoch]=error_train[cur_epoch]+feed(training)
+    error_test[cur_epoch]=error_test[cur_epoch]+test(testing)
+    #5
+    training = iris.iloc[0:120]
+    testing = iris.iloc[120:150]
+    error_train[cur_epoch]=error_train[cur_epoch]+feed(training)
+    error_test[cur_epoch]=error_test[cur_epoch]+test(testing)
+    #mean
+    error_train[cur_epoch]=error_train[cur_epoch]/5
+    error_test[cur_epoch]=error_test[cur_epoch]/5
 
-df = pd.DataFrame({'x1':x1,
-                   'x2':x2,
-                   'x3':x3,
-                   'x4':x4,
-                   'kelas':kelas,
-                   'fakta 1':fakta1,
-                   'fakta 2':fakta2,
-                   'fakta 3':fakta3})
-
-error_training_epoch = np.empty(epoch)
-error_testing_epoch = np.empty(epoch)
-akurasi_epoch = np.empty(epoch)
-
-error_training = 0
-error_testing = 0
-akurasi = 0
-
-text.append("Epoch 0\n")
-text.append("=======\n")
-
-testing = df.iloc[0:30]
-training = df.iloc[30:150]
-
-text.append(str(testing)+"\n")
-
-training_result = back_prop(training, theta_bias_initiation())
-error_training = error_training + training_result[0][0]
-
-testing_result = test(testing,training_result[1])
-error_testing = error_testing + testing_result[0]
-akurasi = akurasi + testing_result[1]
-
-for i in range(epoch):
-    testing = df.iloc[30:60]
-    training = df.iloc[0:30]
-    training = training.append(df.iloc[60:150])
-
-    text.append(str(testing)+"\n")
-    
-    training_result = back_prop(training,training_result[1])
-    error_training = error_training + training_result[0][0]
-    
-    testing_result = test(testing,training_result[1])
-    error_testing = error_testing + testing_result[0]
-    akurasi = akurasi + testing_result[1]
-
-    testing = df.iloc[60:90]
-    training = df.iloc[0:60]
-    training = training.append(df.iloc[90:150])
-
-    text.append(str(testing)+"\n")
-    
-    training_result = back_prop(training,training_result[1])
-    error_training = error_training + training_result[0][0]
-    
-    testing_result = test(testing,training_result[1])
-    error_testing = error_testing + testing_result[0]
-    akurasi = akurasi + testing_result[1]
-
-    testing = df.iloc[90:120]
-    training = df.iloc[0:90]
-    training = training.append(df.iloc[120:150])
-
-    text.append(str(testing)+"\n")
-    
-    training_result = back_prop(training,training_result[1])
-    error_training = error_training + training_result[0][0]
-
-    testing_result = test(testing,training_result[1])
-    error_testing = error_testing + testing_result[0]
-    akurasi = akurasi + testing_result[1]
-
-    testing = df.iloc[120:150]
-    training = df.iloc[0:120]
-    
-    text.append(str(testing)+"\n")
-    
-    training_result = back_prop(training,training_result[1])
-    error_training = error_training + training_result[0][0]
-
-    testing_result = test(testing,training_result[1])
-    error_testing = error_testing + testing_result[0]
-    akurasi = akurasi + testing_result[1]
-
-    mean_error_training = error_training/5
-    mean_error_testing = error_testing/5
-    mean_akurasi = akurasi/5
-    error_training_epoch[i] = mean_error_training
-    error_testing_epoch[i] = mean_error_testing
-    akurasi_epoch[i] = mean_akurasi
-    
-    if(i<epoch-1):
-        text.append("Epoch "+str(i+1)+"\n")
-        
-        error_training = 0
-        error_testing = 0
-        akurasi = 0
-
-        testing = df.iloc[0:30]
-        training = df.iloc[30:150]
-        text.append(str(testing)+"\n")
-        
-        training_result = back_prop(training, training_result[1])
-        error_training = error_training + training_result[0][0]
- 
-        testing_result = test(testing,training_result[1])
-        error_testing = error_testing + testing_result[0]
-        akurasi = akurasi + testing_result[1]
-
-print("Error Training tiap Epoch")
-print(error_training_epoch)
-print("Error Testing tiap Epoch")
-print(error_testing_epoch)
-print("Akurasi tiap Epoch")
-print(akurasi_epoch)
-
-f.writelines(text)
-f.close
-
-plt.plot(error_training_epoch,'r',error_testing_epoch,'g')
-
+fig = plt.figure()
+plt.plot(np.arange(0, epoch), error_test, linestyle='-', label='test')
+plt.plot(np.arange(0, epoch), error_train, color='orange', linestyle='dashed', label='train')
+plt.legend()
+fig.suptitle("Cost Function, alpha = %s" %(a))
+plt.xlabel("Epoch #")
+plt.ylabel("Loss")
 plt.show()
